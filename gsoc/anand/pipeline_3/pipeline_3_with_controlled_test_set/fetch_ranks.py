6eccfaca9a76a5c62495f5989c1b106fb8405c82
panchbhai1969 <panchbhai1969@gmail.com>
2019-06-25 16:30:45 +0530

    Controlled test set model Prepared | Code Improvements

---
+++ gsoc/anand/pipeline_3/pipeline_3_with_controlled_test_set/fetch_ranks.py
@@ -0,0 +1,16 @@
+from tqdm import tqdm
+import numpy as np
+
+def fetch_ranks(filename='../utility/wikidata.rank'):
+    sub = open(filename,'r').readlines()
+    diction={}
+
+    print("Loading Rankings")
+    for val in tqdm(sub):
+        diction[val.split('\t')[0]] = float(val.split('\t')[1])
+    return diction
+
+if __name__ == "__main__":
+    fetch_ranks()
+    pass
+
