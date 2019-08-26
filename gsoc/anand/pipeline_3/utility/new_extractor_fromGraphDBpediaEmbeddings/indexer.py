28c01e56734f0105f42fb40a6c26966ec510f8f5
Anand Panchbhai <panchbhai1969gmail.com>
2019-08-26 19:24:07 +0530

    readme updated

--- gsoc/anand/pipeline_3/utility/new_extractor_fromGraphDBpediaEmbeddings/indexer.py
+++ gsoc/anand/pipeline_3/utility/new_extractor_fromGraphDBpediaEmbeddings/indexer.py
@@ -1,11 +1,19 @@
+"""
+This code extracts all the embeddings from the pageRank.txt and records the following information in the following manner.
+
+<name of embedding>\t<line number>\t<File number> 
+
+This index will contain information regarding the position of all the words so that the corresponding embeddings can be extracted easily. Withput having to query through the whole embedding file.
+"""
+
 import sys
 import os
 from tqdm import tqdm 
 
-a = [f for f in os.listdir(".") if f.startswith("xa")]
+a = [f for f in os.listdir("data_fragments/")]
 for files in tqdm(a):
     #print(files)
-    lines = open(files).readlines()
+    lines = open("data_fragments/"+files).readlines()
     writer = open("index.csv",'a')
     for line in range(len(lines)):
         lines[line] = lines[line].split("\t")
